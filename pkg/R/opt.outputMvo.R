6237a2309faabe48d81115ba41d5ccbf36d1f8ca
martinrd (Doug Martin) <martinrd@comcast.net>
2014-07-13 23:23:24 +0000

    New function edited.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mpo@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/opt.outputMvo.R
+++ pkg/R/opt.outputMvo.R
@@ -1,20 +1,21 @@
-opt.outputMeanVol <-
-function(opt,returns,digits = NULL,names = NULL)
+opt.outputMvo <-
+function(opt,returns,digits = NULL,names = NULL,rf = 0)
 {
 wts = opt$weights
 sigmasq = as.numeric(t(wts)%*%var(returns)%*%wts)
 sigma = sqrt(sigmasq)
 mu.ret = apply(returns,2,mean)
 mu = as.numeric(t(wts)%*%mu.ret)
+sr = (mu-rf)/sigma
 if(is.null(digits))
     {names(sigma) = "sigma"
     names(mu) = "mu"
     output = c(wts,mu,sigma)} else
     {if(is.null(names))
-        {output = list(wts=wts,mean=mu,vol=sigma)
+        {output = list(wts=wts,mean=mu,stdev=sigma,sr=sr)
          output = lapply(output,round,digits)}
          else
-        {output = list(wts,mu,sigma)
+        {output = list(wts,mu,sigma,sr)
          names(output) = names
          output = lapply(output,round,digits)}
     }

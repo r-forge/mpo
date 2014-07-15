5be4d5e677144c0ba483c30eaff1778ac2de8fb9
martinrd (Doug Martin) <martinrd@comcast.net>
2014-07-15 21:05:50 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mpo@19 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/opt.outputMvoWts.R
@@ -0,0 +1,19 @@
+opt.outputMeanVolWts = function(opt,returns,digits = NULL,names = NULL)
+{
+wts = opt$weights
+sigmasq = as.numeric(t(wts)%*%var(returns)%*%wts)
+sigma = sqrt(sigmasq)
+mu.ret = apply(returns,2,mean)
+mu = as.numeric(t(wts)%*%mu.ret)
+if(is.null(digits))
+    {output = wts} else
+    {if(is.null(names))
+        {output = list(wts=wts)
+         output = lapply(output,round,digits)}
+         else
+        {output = list(wts=wts)
+         names(output) = names
+         output = lapply(output,round,digits)}
+    }
+output
+}

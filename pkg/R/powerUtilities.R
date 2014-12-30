8f174060ef149194e4dc24d95ba68c7613505b39
martinrd (Doug Martin) <martinrd@comcast.net>
2014-12-30 23:15:50 +0000

    New function to make power utility functions plots.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mpo@33 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/powerUtilities.R
@@ -0,0 +1,19 @@
+powerUtilities = function()
+{
+  x = seq(.01,3,.01)
+  lwd = 2
+  y = log(x)
+  pdf(file="powerUtilityFncs.pdf", width=8, height=6)
+  plot(x,y,type = "l",ylim =c(-8,2),lwd = lwd,xlab = "WEALTH V",ylab = "U(V)")
+  gamma = -1
+  y = (x^gamma - 1)/gamma
+  lines(x,y,lty = 8,lwd = lwd)
+  gamma = .5
+  y = (x^gamma - 1)/gamma
+  lines(x,y,lty = 3,lwd = lwd)
+  abline(v = 0)
+  legend(1.2,-5.5,c("Gamma  =  .5", "Log Utility","Gamma  =  -1"),
+       lty = c(3,1,8),lwd = 2)
+  dev.off()
+}
+powerUtilities()

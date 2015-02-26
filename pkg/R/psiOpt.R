b5997e206d544282996a7019a77d3dc115de5684
martinrd (Doug Martin) <martinrd@comcast.net>
2015-02-26 22:06:10 +0000

    New functions rhoOpt, psiOpt and wtsOpt for plotting optimal rho, psi and wts for robust regression.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mpo@46 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/psiOpt.R
@@ -0,0 +1,37 @@
+psiOpt = function(r,c)
+{
+  n = length(r)
+  psi = rep(0,n)
+  g1 = -1.944
+  g3 = 1.728
+  g5 = -.312
+  g7 = .016
+  c2 = c^2
+  k = c/3
+  k2 = k^2
+  for(i in 1:n)
+  {
+    x = r[i]/k
+    x3 = x^3
+    x5 = x^5
+    x7 = x^7
+    if((abs(x)>2) & (abs(x)<=3))
+    {psi[i] = k*(g1*x+g3*x3+g5*x5+g7*x7)}
+    else
+    {if(abs(x)<=2) 
+    {psi[i] = r[i]} else {psi[i] = 0}}
+  }
+  psi
+}
+r = seq(-5,5,.01)
+c90 = 2.832
+c95 = 3.18
+c99 = 3.87
+y90 = psiOpt(r,c90)
+y95 = psiOpt(r,c95)
+y99 = psiOpt(r,c99)
+plot(r,y99,type = "l",xlab="",ylab = "PSI(r)",lty = 3)
+lines(r,y95,lwd = 1.5)
+lines(r,y90,lty = 2)
+eff.text = c("99 %","95 %","90 %")
+legend("topleft",lty=c(3,1,2),eff.text,bty = "n")

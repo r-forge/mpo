f0cf8671343403e4f95201f30b10706cd30b4840
martinrd (Doug Martin) <martinrd@comcast.net>
2015-03-21 14:24:58 +0000

    Fixed tsPlot.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mpo@63 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/tsPlot.R
+++ pkg/R/tsPlot.R
@@ -2,7 +2,7 @@ tsPlot = function(ret,add.grid = T,cex = 1.0)
 {
   if(add.grid) {type = c("l","g")} else
   {type = "l"}
-  xyplot(100*retVHI[,1:2],par.strip.text = list(cex = cex),type = type,
+  xyplot(100*ret,par.strip.text = list(cex = cex),type = type,
          xlab = "", ylab = list(label="RETURNS (%)",cex = cex),
          scales = list(y = list(cex = cex),x = list(cex = cex) )) 
 }

5b9da89e15927ff908a2de9f2dce5ec189f5ce2b
kecoli (Student ) <kirkli@uw.edu>
2016-06-02 06:30:38 +0000

    minor fix
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mpo@104 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/table.Performance.pool.R
+++ pkg/R/table.Performance.pool.R
@@ -47,6 +47,7 @@ table.Performance.pool <- function(...){
 			"sd.multiperiod", 
 			"SemiDeviation", 
 			"SemiVariance", 
+			"SFM.beta",
 #			"SharpeRatio",
 			"sharpeRatio",
 			"SharpeRatio.annualized", 
@@ -65,3 +66,45 @@ table.Performance.pool <- function(...){
 			"VaR", 
 			"VolatilitySkewness")
 }
+
+.beta <- function (xRa, xRb, subset) 
+{
+	if (missing(subset)) 
+		subset <- TRUE
+	if (NCOL(xRa) != 1L || NCOL(xRb) != 1L || NCOL(subset) != 
+			1L) 
+		stop("all arguments must have only one column")
+	merged <- as.data.frame(na.omit(cbind(xRa, xRb, subset)))
+	if (NROW(merged) == 0) 
+		return(NA)
+	colnames(merged) <- c("xRa", "xRb", "subset")
+	merged$subset <- as.logical(merged$subset)
+	model.lm = lm(xRa ~ xRb, data = merged, subset = merged$subset)
+	beta = coef(model.lm)[[2]]
+	beta
+}
+
+SFM.beta <- function (R, Rb.index = 1, Rf = 0){
+	Ra = checkData(R)
+	Rb = R[,Rb.index]
+	Rb = checkData(Rb)
+	if (!is.null(dim(Rf))) 
+		Rf = checkData(Rf)
+	Ra.ncols = NCOL(Ra)
+	Rb.ncols = NCOL(Rb)
+	xRa = Return.excess(Ra, Rf)
+	xRb = Return.excess(Rb, Rf)
+	pairs = expand.grid(1:Ra.ncols, 1:Rb.ncols)
+	result = apply(pairs, 1, FUN = function(n, xRa, xRb) .beta(xRa[, 
+								n[1]], xRb[, n[2]]), xRa = xRa, xRb = xRb)
+	if (length(result) == 1) 
+		return(result)
+	else {
+		dim(result) = c(Ra.ncols, Rb.ncols)
+		colnames(result) = paste("Beta:", colnames(Rb))
+		rownames(result) = colnames(Ra)
+		return(t(result))
+	}
+}
+
+
